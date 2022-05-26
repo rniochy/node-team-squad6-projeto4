895d0a02577206a56bdd4310cbf263f8796fab19
rniochy <lniochy@gmail.com>
2022-05-26 12:39:46 +0100

    improve securuty into app

--- src/infraestructure/routes/user.routeAdapater.ts
+++ src/infraestructure/routes/user.routeAdapater.ts
@@ -7,32 +7,23 @@ import UserRepository from "../../domain/repository/userRepository";
 export default class UserRoutes implements IUserRoute{
    async getRouters(userRepository:UserRepository):Promise<Router> {
          const router = Router()
-         router.post('/create', (req: Request, res: Response)=>{
+         router.post('/create', async (req: Request, res: Response)=>{
              try {
-                    const {fullname, email, password} = req.body
-                    const createUser =  new CreateUser(userRepository)
-                    
-                        createUser.perform({fullname, email, password})
+                const {fullname, email, password} = req.body
+                const createUser =  new CreateUser(userRepository)
+                
+                const  userExist = await userRepository.getUser(email)
+
+                console.log(userExist.email)
+                createUser.perform({fullname, email, password})
                         .then(result => res.status(200).send({sucess: "user createted!"})) 
                         .catch(err=> res.status(400).send({error: "Didn't create user!"}))
 
              } catch(err){ res.status(500).send({error: "Error to create user"})}            
             })
 
-        router.post('/a', (req: Request, res: Response)=>{
-                console.log(req.body)
-                return res.send("Result A")
-     });
      return router     
 }
 
 
 } 
-
-
-// const router = Router()
-// router.post('/u', (req: Request, res: Response)=>{
-//                console.log(req.body)
-//                return res.send("Result")
-//     });
-// export  router 
